e007fecd97acbaa5d0f446effdb0001044190bcf
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-07-18 09:58:18 -0600

    Fixed iPad error by renaming iPhone.xib to ViewController.xib and updating didFinishLaunchingWithOptions

--- AOC2Project2/AOC2Project2/AppDelegate.m
+++ AOC2Project2/AOC2Project2/AppDelegate.m
@@ -26,7 +26,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+    /*self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
         self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController_iPhone" bundle:nil] autorelease];
@@ -35,7 +41,7 @@
     }
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
-    return YES;
+    return YES;*/
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
