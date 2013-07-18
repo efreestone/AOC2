e7cd7da3ae2a2b4a551a47abae79bd7792426333
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-07-17 21:06:13 -0600

    Set up SecondView with .xib, .h and .m. Present and dismissViewController both working

---
+++ AOC2Project2/AOC2Project2/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewViewController.h
+//  AOC2Project2
+//
+//  Created by Elijah Freestone on 7/17/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewViewController : UIViewController
+
+@end
---
+++ AOC2Project2/AOC2Project2/SecondViewController.m
@@ -0,0 +1,38 @@
+//
+//  SecondViewViewController.m
+//  AOC2Project2
+//
+//  Created by Elijah Freestone on 7/17/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "SecondViewViewController.h"
+
+@interface SecondViewViewController ()
+
+@end
+
+@implementation SecondViewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
