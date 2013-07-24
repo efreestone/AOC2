33ab98a7744799397bb9e3fdd7ac18015f81d706
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-07-23 20:18:25 -0600

    Added second view (AddEventView) and onClick function to display it

---
+++ AOC2Project3/AOC2Project3/AddEventView.h
@@ -0,0 +1,13 @@
+//
+//  AddEventView.h
+//  AOC2Project3
+//
+//  Created by Elijah Freestone on 7/23/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddEventView : UIViewController
+
+@end
---
+++ AOC2Project3/AOC2Project3/AddEventView.m
@@ -0,0 +1,38 @@
+//
+//  AddEventView.m
+//  AOC2Project3
+//
+//  Created by Elijah Freestone on 7/23/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "AddEventView.h"
+
+@interface AddEventView ()
+
+@end
+
+@implementation AddEventView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
