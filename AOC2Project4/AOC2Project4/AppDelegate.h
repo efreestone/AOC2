f394458d3867ab46e2719fc58d52458a012e570f
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-07-27 11:18:34 -0600

    Initial commit for Project 4 with new single view

---
+++ AOC2Project4/AOC2Project4/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AOC2Project4
+//
+//  Created by Elijah Freestone on 7/27/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AOC2Project4/AOC2Project4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC2Project4
+//
+//  Created by Elijah Freestone on 7/27/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
