4ee1daf9b60f4f4e3f1c4f185511ec5332492dd7
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-07-31 14:36:05 -0600

    Added AddEventView and changed main view to only use iphone.xib

---
+++ AOC2Project4/AOC2Project4/AddEventView.h
@@ -0,0 +1,13 @@
+//
+//  AddEventViewViewController.h
+//  AOC2Project4
+//
+//  Created by Elijah Freestone on 7/31/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddEventViewViewController : UIViewController
+
+@end
---
+++ AOC2Project4/AOC2Project4/AddEventView.m
@@ -0,0 +1,38 @@
+//
+//  AddEventViewViewController.m
+//  AOC2Project4
+//
+//  Created by Elijah Freestone on 7/31/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "AddEventViewViewController.h"
+
+@interface AddEventViewViewController ()
+
+@end
+
+@implementation AddEventViewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
