a8f2d85a6c93ccec4020c5c08073c4e0a951f6f7
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-07-30 20:51:26 -0600

    Created singleton. Havent worked out exactly how I will use it yet though

---
+++ AOC2Project4/AOC2Project4/EventSingleton.h
@@ -0,0 +1,13 @@
+//
+//  EventSingleton.h
+//  AOC2Project4
+//
+//  Created by Elijah Freestone on 7/30/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventSingleton : NSObject
+
+@end
---
+++ AOC2Project4/AOC2Project4/EventSingleton.m
@@ -0,0 +1,13 @@
+//
+//  EventSingleton.m
+//  AOC2Project4
+//
+//  Created by Elijah Freestone on 7/30/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "EventSingleton.h"
+
+@implementation EventSingleton
+
+@end
